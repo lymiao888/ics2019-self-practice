334ba464abda22d90ea156012bd00f40c924f66d
tracer-ics2019 <tracer@njuics.org>
2024-02-12 14:08:17 +0800

    >  compile 181220000 lym Linux lym 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  14:08:17 up  3:25,  0 user,  load average: 0.00, 0.00, 0.00 f012bfa3e665fb1b903a1e5c3928fc58c2f36733

--- README.md
+++ README.md
@@ -2,14 +2,23 @@
 NJU PA 2019
 the Intrduction is [here](https://nju-projectn.github.io/ics-pa-gitbook/ics2019/)
 
-##PA1
-Enter nemu to run
-'''
+## PA1
+### 准备阶段
+安装相关模块后，进入.\nemu运行
+```
 make ISA=$ISA run
-'''
+```
 * for x86: I update the CPU state in nemu/src/isa/x86/include/isa/reg.h
 
+### 实现简易调试器
 
+#### 单步执行
+#### 打印寄存器
+#### 扫描内存
+
+### 表达式求值
+
+### 监视点 
 
 
 
--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -38,6 +38,15 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args) {
+/* extract the first argument */
+  //char *arg = strtok(NULL, " ");
+  printf("start single step! ");
+  
+  return 0;
+}
+
+
 static struct {
   char *name;
   char *description;
@@ -46,7 +55,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si [N]","Let the program execute N instructions in a single step and then pause execution.N default = 1", cmd_si },
   /* TODO: Add more commands */
 
 };
